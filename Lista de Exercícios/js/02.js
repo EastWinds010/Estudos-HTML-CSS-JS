0c95c9dddb3c17b5b40a07338822e69a3ef94ea2
Rafael Lucas <rafaelluc2000@gmail.com>
2022-03-30 02:52:23 -0300

    Create 02.js

---
+++ Lista de Exercícios/js/02.js
@@ -0,0 +1,9 @@
+// Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+function E2() {
+    const n = window.prompt("Digite um número número");
+    if (n > 0) {
+      Resposta2.value = ("O Número " + n + " é POSITIVO")
+    } else {
+      Resposta2.value = ("O Número " + n + " é NEGATIVO")
+    }
+  }
