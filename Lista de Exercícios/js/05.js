2785d2889e57b94575bbf8828d9ab82b65a72527
Rafael Lucas <rafaelluc2000@gmail.com>
2022-03-30 02:52:34 -0300

    Create 05.js

---
+++ Lista de Exercícios/js/05.js
@@ -0,0 +1,18 @@
+// Faça um programa para a leitura de duas notas parciais de um aluno. 
+// O programa deve calcular a média alcançada 
+// por aluno e apresentar:
+// A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+// A mensagem "Reprovado", se a média for menor do que sete;
+// A mensagem "Aprovado com Distinção", se a média for igual a dez.
+function E5() {
+    const n1 = parseInt(window.prompt("Digite a primeira nota: "));
+    const n2 = parseInt(window.prompt("Digite a segunda nota: "));
+    let media = (n1+n2)/2;
+    if(media == 10){
+      Resposta5.value = ("Aprovado com Distinção");
+    }else if(media >= 7 && media < 10){
+      Resposta5.value = ("Aprovado")
+    }else{
+      Resposta5.value = ("Reprovado")
+    }
+  }
