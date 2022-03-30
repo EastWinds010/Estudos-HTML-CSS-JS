ad568c34febd022e8640bbe8954a8adf1b9161f3
Rafael Lucas <rafaelluc2000@gmail.com>
2022-03-30 02:52:18 -0300

    Create 01.js

---
+++ Lista de Exercícios/js/01.js
@@ -0,0 +1,10 @@
+// Faça um Programa que peça dois números e imprima o maior deles.
+function E1() {
+    const a = window.prompt("Digite o primeiro número");
+    const b = window.prompt("Digite o segundo número");
+    if (a > b) {
+      Resposta1.value = (a);
+    } else {
+      Resposta1.value = (b);
+    }
+  }
