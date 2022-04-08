468e385197be2364257fb2f12e04aed987cadd4e
Rafael Lucas <88330283+EastWinds010@users.noreply.github.com>
2022-04-08 19:44:33 -0300

    Exercícios de Repetição

--- Exercícios de Repetição/06.js
+++ Exercícios de Repetição/06.js
@@ -1,8 +1,9 @@
+//INCOMPLETO
+
 // Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o 
 // programa para que ele mostre os números um ao lado do outro.
 
 // for(i= 1; i<21; i++ ){
 //     console.log(i)
 // }
-
 
--- Exercícios de Repetição/07.js
+++ Exercícios de Repetição/07.js
@@ -1,1 +1,5 @@
-console.log("oi")
+// Faça um programa que leia 5 números e informe o maior número.
+for(let i = 1; i <6; i++){
+    let n = console.log("Digite um número "+i+"/5");
+    
+}
